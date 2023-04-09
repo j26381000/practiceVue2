7a64d39417cc1c1e22e84b1c1b5692f1a53ca629
j26381000 <j82860@gmail.com>
2023-04-09 16:36:02 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:RexHung0302/Hexschool-firebase-bmi-practice.git master:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
-git push -f https://github.com/j26381000/practiceVue.git master:gh-pages
+git push -f https://github.com/j26381000/practiceVue2.git master:gh-pages
 
 cd -
--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <style lang="scss">
-//@import '~bootstrap/scss/bootstrap';
+@import '~bootstrap/scss/bootstrap';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
