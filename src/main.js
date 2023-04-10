da132395d31442c49e079000fe3842cbca209f7c
j26381000 <j82860@gmail.com>
2023-04-10 21:31:29 +0800

    update

--- src/main.js
+++ src/main.js
@@ -8,9 +8,12 @@ import VueAxios from 'vue-axios'
 import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/css/index.css'
 import store from './store'
+import Vuex from 'vuex'
 
 createApp(App).use(store)
   .use(router)
+  .use(store)
+  .use(Vuex)
   .component('Loading', Loading)
   .use(VueAxios, axios)
   .mount('#app')
